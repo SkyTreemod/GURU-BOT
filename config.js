7078ce76b81af39483256fbafd089f806e0f3259
SkyTreemod <134457212+SkyTreemod@users.noreply.github.com>
2023-06-10 14:01:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['51983750133', 'GURU', true],
+  ['51983750133', 'Ash', true], 
   [''] 
 ] //Number of owners
 
